444835cc9fa0187e289333da78d905a2261582f0
DhamaAnkur <akdhama98@gmail.com>
2022-11-17 10:08:55 +0530

    added

--- DSA/String_Demo/StringDemo.java
+++ DSA/String_Demo/StringDemo.java
@@ -1,7 +1,11 @@
 
+
+import java.util.*;
 public class StringDemo {
 
    public static void main(String[] args) {
+     Scanner scn = new Scanner (System.in);
+
 
       // create string
       String str = "aman";
@@ -21,20 +25,32 @@ public class StringDemo {
       // get character
 
       char ch =  str.charAt(3);
-      System.out.println("index value of  3 " + ch);
+      System.out.println("index value of  3 is --> " + ch);
 
      // concatination 
 
-     System.out.print("My" + " name " + " is " + " Aman " + " Srivastava ");
+     System.out.println("My" + " name " + " is " + " Aman " + " Srivastava ");
 
-     String a = "aman";
-     String b = "pepcoder";
-     String c  = new String();
-     c = a+b;
-  
-   c  = "aman pepcoder";
-   //   System.out.println(c);
+     String a = "Aman";
+     String b = " pepcoder";
+     String c  = a+b;;
+   
+     System.out.println(c);
 
+
+   // Input String for full length and single String
+
+   // System.out.println("Enter your input here--> ");
+   // String x = scn.nextLine();
+   // System.out.println(x);
+   
+
+   // char input 
+
+   System.out.println("Enter your string ");
+   char firstChar = scn.next().charAt(0);
+   System.out.println("Your 0 index char is --> ");
+   System.out.println(firstChar);
 
 
    }
