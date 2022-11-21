1891266d599aa8d6d0de8ce5b3cef65db2faa333
DhamaAnkur <akdhama98@gmail.com>
2022-11-21 13:53:51 +0530

    zigzag

---
+++ DSA/Recursion/Print_Zig_Zag/Main.java
@@ -0,0 +1,27 @@
+
+//Print Zig Zag using recursion
+//Link of Question ( https://www.pepcoding.com/resources/online-java-foundation/introduction-to-recursion/print-zig-zag-official/ojquestion )
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn =new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==0)
+        return ;
+        
+        System.out.print(n+" ");     // it is work for leftside(Pre)
+        pzz(n-1);
+        System.out.print( n+" ");    // it is work for between(In)
+        pzz(n-1);
+        System.out.print(n+" ");     // it is work for rightside(Post)
+    }
+
+}
