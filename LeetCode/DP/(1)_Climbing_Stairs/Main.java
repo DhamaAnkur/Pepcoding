929a002fed4d7e024a99ec2b710a56d6cbf91904
DhamaAnkur <akdhama98@gmail.com>
2022-12-11 23:50:45 +0530

    Leetcode

---
+++ LeetCode/DP/(1)_Climbing_Stairs/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void main (String [] args ){ 
+         int n = scn.nextInt();  
+         int []memo= new int[n+1];
+         int ans  = rec(n  , memo);
+         System.out.println(ans);
+    }
+
+    private static int rec(int n , int[]memo){
+        if(n <= 1){
+            return 1;
+        }
+
+        if(memo[n] > 0){
+            return memo[n];
+        }
+
+           int opt1 = rec(n-1 , memo);
+           int opt2 = rec(n-2 , memo);
+
+           memo[n] = opt1 + opt2;
+           
+           return opt1 + opt2;
+    }
+}
+
