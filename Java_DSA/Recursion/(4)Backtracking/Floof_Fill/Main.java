4925562982b86445a2c236f1269c53a9ea4af260
DhamaAnkur <akdhama98@gmail.com>
2022-12-20 19:07:01 +0530

    Flood_fill_4

--- Java_DSA/Recursion/(4)Backtracking/Floof_Fill/Main.java
+++ Java_DSA/Recursion/(4)Backtracking/Floof_Fill/Main.java
@@ -141,3 +141,59 @@ public class Main {
  * }
  * }
  */
+
+ 
+/*---------------------------------------------------------------------------------------------------------- */
+// solution number - (4)
+
+/*
+ * import java.io.*;
+ * import java.util.*;
+ * 
+ * public class Main {
+ * 
+ * public static void main(String[] args) {
+ * Scanner scn = new Scanner(System.in);
+ * int n = scn.nextInt();
+ * int m = scn.nextInt();
+ * int[][] arr = new int[n][m];
+ * for (int i = 0; i < n; i++) {
+ * for (int j = 0; j < m; j++) {
+ * arr[i][j] = scn.nextInt();
+ * }
+ * }
+ * 
+ * floodfill(arr, 0, 0, "");
+ * }
+ * 
+ * static int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
+ * static String[]dirN = {"t","l","d","r"};
+ * 
+ * // asf -> answer so far
+ * public static void floodfill(int[][] maze, int sr, int sc, String asf) {
+ * 
+ * if(maze[sr][sc]==1)
+ * return;
+ * 
+ * if(sr == maze.length-1 && sc == maze[0].length-1){
+ * System.out.println(asf);
+ * return ;
+ * }
+ * 
+ * maze[sr][sc] = 1 ;
+ * 
+ * for(int d=0; d<dir.length ; d++){
+ * int r = sr+dir[d][0];
+ * int c = sc+dir[d][1];
+ * 
+ * if(r>=0 && c>=0 && r<maze.length && c<maze[0].length){
+ * floodfill(maze,r,c,asf+dirN[d]);
+ * }
+ * 
+ * }
+ * 
+ * maze[sr][sc] = 0 ;
+ * 
+ * }
+ * }
+ */
