9948b31dcb3284d4dd8f1a2ab49fba1752ee1096
DhamaAnkur <akdhama98@gmail.com>
2022-12-29 14:26:47 +0530

    Remove_Last

--- Java_DSA/LinkedList/Basic/Remove_Last/Main.java
+++ Java_DSA/LinkedList/Basic/Remove_Last/Main.java
@@ -87,6 +87,24 @@ public class Main {
         return temp.data;
       }
     }
+    
+      public Node getNodeAt(int idx) {
+      if (size == 0) {
+        System.out.println("List is empty");
+        // return -1;
+      } else if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+        // return -1;
+      } 
+      
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+          temp = temp.next;
+        }
+        return temp;
+      
+       
+    }
 
     public void addFirst(int val) {
       Node temp = new Node();
@@ -124,14 +142,20 @@ public class Main {
     }
 
     public void removeLast(){
-      Node temp =  head ;
-      
-      for(int i=0; i<size-2; i++){
-          temp = temp.next;
-      }
-      
-      temp.next = null ;
-      tail = temp ;
+     if(size ==0 ){
+         System.out.println("List is empty");
+     }
+     
+     else if(size ==1){
+         head = null;
+         tail = null;
+         size--;
+     }
+     
+      int idx = size-1;
+      Node temp = getNodeAt(idx-1);
+      temp.next = null;
+      tail =temp;
       size--;
     }
   }
