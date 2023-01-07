3e486734279aa098f4f88870594dc289d8f77c93
DhamaAnkur <akdhama98@gmail.com>
2023-01-07 23:36:22 +0530

    Adapters

---
+++ Java_DSA/LinkedList/Medium/LL_to_stackAdapter/Main.java
@@ -0,0 +1,61 @@
+package Java_DSA.LinkedList.Medium.LL_to_stackAdapter;
+
+// Linked List To Stack Adapter
+// Link of Question ( https://www.pepcoding.com/resources/online-java-foundation/linked-lists/linkedlist-to-stack-adapter-official/ojquestion )
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class LLToStackAdapter {
+    LinkedList<Integer> list;
+
+    public LLToStackAdapter() {
+      list = new LinkedList<>();
+    }
+
+
+    int size() {
+      return list.size();
+    }
+
+    void push(int val) {
+      list.addFirst(val);
+    }
+
+    int pop() {
+      return list.removeFirst();
+    }
+
+    int top() {
+      return list.getFirst();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    LLToStackAdapter st = new LLToStackAdapter();
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("push")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if(str.startsWith("pop")){
+        int val = st.pop();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("top")){
+        int val = st.top();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("size")){
+        System.out.println(st.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
