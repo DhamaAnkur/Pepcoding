7378daf9cd0a005c1ff066b33889287b3b3d1c74
DhamaAnkur <akdhama98@gmail.com>
2023-01-21 17:40:00 +0530

    Leetcode125

---
+++ LeetCode/String/Leetcode125/Main.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        s = s.toLowerCase();
+
+        for(int i=0; i<s.length(); i++){
+            if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }
+        }
+
+        int j = sb.length()-1;
+
+        for(int i=0; i<sb.length()/2; i++){
+            if(sb.charAt(i) != sb.charAt(j)){
+                return false;
+            }
+            j--;
+        }
+
+        return true;
+
+    }
+}
