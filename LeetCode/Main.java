21e5e183737d5470085757b98be549adbb1a6856
DhamaAnkur <akdhama98@gmail.com>
2023-02-17 23:35:14 +0530

    changes

---
+++ LeetCode/Main.java
@@ -0,0 +1,32 @@
+// package LeetCode;
+
+public class Main {
+
+    // public static void convert(int a){
+    //     String ans ="";
+    //     while(a!=0){
+    //         ans += a%10;
+    //         a /= 10;
+    //     }
+    //     System.out.println(ans);
+    // }
+    
+    public static void main(String[] args) {
+        String s = "123";
+        String b = "456";
+
+        int x = Integer.parseInt(s);
+        int y = Integer.parseInt(b);
+
+       int a = x*y ;
+
+        StringBuilder ans = new StringBuilder();
+        while(a!=0){
+            ans.insert(0 , a%10);
+            a /= 10;
+        }
+        System.out.println(ans);
+
+
+    }
+}
