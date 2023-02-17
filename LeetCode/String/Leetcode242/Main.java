b0d5002aa8bf1e32595b28f65f3acda89a086479
DhamaAnkur <akdhama98@gmail.com>
2023-02-17 22:00:12 +0530

    ValidAnagram

--- LeetCode/String/Leetcode242/Main.java
+++ LeetCode/String/Leetcode242/Main.java
@@ -1,37 +1,36 @@
 
-//Leetcode 242 Valid Anagram
-//Link of this question( https://leetcode.com/problems/valid-anagram/ )
+//Leetcode 241 Valid Anagaram
+//Link of The Question( https://leetcode.com/problems/valid-anagram/ )
+
 import java.util.*;
-
 class Main {
-    public boolean isAnagram(String a, String b) {
-        if(a.length() != b.length()){
-            return false;
-        }
-    HashMap<Character,Integer> hm  = new HashMap<>();
+    public boolean isAnagram(String s, String t) {
+        
+        HashMap<Character , Integer> hm = new HashMap<>();
 
-     for(int i=0; i<a.length(); i++){
-        char ch = a.charAt(i);
-        hm.put(ch , hm.getOrDefault(ch , 0) + 1);
-     }
-
-    HashMap<Character,Integer> hm2  = new HashMap<>();
-    for(int i=0; i<b.length(); i++){
-        char ch = b.charAt(i);
-        hm2.put(ch , hm2.getOrDefault(ch , 0) + 1);
-     }
- 
-    for(int i=0; i<b.length(); i++){
-        char ch = b.charAt(i);
-        if(hm.containsKey(ch)){
-            continue;
+        for(int i=0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+            hm.put(ch , hm.getOrDefault(ch , 0) +1);
         }
-        else{
-            return false;
-        }
-    }
 
-    return true; 
+         for(int i=0 ; i<t.length(); i++){
+            char ch = t.charAt(i);
+            
+            if(hm.containsKey(ch) == false){
+                return false;
+            }
+
+            else if(hm.get(ch) == 1){
+                hm.remove(ch);
+            }
+        
+            else{
+                hm.put(ch , hm.get(ch)-1);
+            }
+
+        }
+
+    return hm.size() == 0;
 
     }
 }
