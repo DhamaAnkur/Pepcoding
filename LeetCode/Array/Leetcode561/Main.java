c651552838878580301ac4e074a23266f8c462bc
DhamaAnkur <akdhama98@gmail.com>
2023-02-19 23:54:56 +0530

    Leetcode561

---
+++ LeetCode/Array/Leetcode561/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class Main {
+
+    public int arrayPairSum(int[] arr) {
+    Arrays.sort(arr);
+    int i =0;
+    int j=1;
+    int sum = 0;
+
+    while(i<=arr.length && j<=arr.length){
+        int a = arr[i];
+        int b = arr[j];
+        sum += Math.min(a , b);
+        i = i+2;
+        j = j+2;
+
+    }
+
+    return sum ;
+
+
+    }
+}
